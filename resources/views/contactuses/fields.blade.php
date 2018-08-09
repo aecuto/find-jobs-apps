ae512501b4483853c281a46dab9ee1037bb49af8
aquzohr <kampee.expertly01@gmail.com>
2018-08-09 16:43:45 +0700

    remove cancel

--- resources/views/contactuses/fields.blade.php
+++ resources/views/contactuses/fields.blade.php
@@ -42,7 +42,6 @@
   <!-- Submit Field -->
   <div class="form-group col" style="margin: auto;">
     {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('contactuses.index') !!}" class="btn btn-default">Cancel</a>
   </div>
 </div>
 
--- resources/views/payment_notifications/fields.blade.php
+++ resources/views/payment_notifications/fields.blade.php
@@ -58,7 +58,6 @@
   <!-- Submit Field -->
   <div class="form-group col" style="margin: auto;">
     {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('paymentNotifications.index') !!}" class="btn btn-default">Cancel</a>
   </div>
 
 </div>
